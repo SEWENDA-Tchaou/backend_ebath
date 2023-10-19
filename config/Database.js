333b6c45abcd6dc60be0c5ef926f9ec9bdf04ffd
sewenda tchaou <sewendatchaou@gmail.com>
2023-10-18 17:11:42 -0700

    backend

---
+++ config/Database.js
@@ -0,0 +1,11 @@
+// const { Sequelize } = require("sequelize");
+
+import { Sequelize } from "sequelize";
+
+const db = new Sequelize ('ebath_btp', 'root', '', {
+    host:'localhost',
+    dialect:'mysql'
+});
+
+export default db;
+// module.exports = db;
